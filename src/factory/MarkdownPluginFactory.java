81001966acf31b2445ff486a955cbe90b0cfc34f
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-12-02 22:14:38 +0300

    add singleton implementation of MarkdownPluginFactory

--- src/factory/MarkdownPluginFactory.java
+++ src/factory/MarkdownPluginFactory.java
@@ -2,15 +2,23 @@ package factory;
 
 import editor.PluginTextEditor;
 
-public class MarkdownPluginFactory extends PluginFactory{
-   
-    public MarkdownPluginFactory(PluginTextEditor editor) {
+public class MarkdownPluginFactory extends PluginFactory {
+    private static MarkdownPluginFactory instance;
+
+    private MarkdownPluginFactory(PluginTextEditor editor) {
         super(editor);
+    }
+
+    public static synchronized MarkdownPluginFactory getInstance(PluginTextEditor editor) {
+        if (instance == null) {
+            instance = new MarkdownPluginFactory(editor);
+        }
+        return instance;
     }
 
     @Override
     public Plugin createPlugin() {
         return new MarkdownPlugin(editor);
     }
-
 }
+
