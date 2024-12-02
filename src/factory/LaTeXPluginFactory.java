04448fe2f80d63a6f302b2fc58ee880d34549644
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-12-02 22:14:27 +0300

    add singleton implementation of LaTeXPluginFactory

--- src/factory/LaTeXPluginFactory.java
+++ src/factory/LaTeXPluginFactory.java
@@ -2,10 +2,18 @@ package factory;
 
 import editor.PluginTextEditor;
 
-public class LaTeXPluginFactory extends PluginFactory{
-       
-    public LaTeXPluginFactory(PluginTextEditor editor) {
+public class LaTeXPluginFactory extends PluginFactory {
+    private static LaTeXPluginFactory instance;
+
+    private LaTeXPluginFactory(PluginTextEditor editor) {
         super(editor);
+    }
+
+    public static synchronized LaTeXPluginFactory getInstance(PluginTextEditor editor) {
+        if (instance == null) {
+            instance = new LaTeXPluginFactory(editor);
+        }
+        return instance;
     }
 
     @Override
@@ -13,3 +21,4 @@ public class LaTeXPluginFactory extends PluginFactory{
         return new LaTeXPlugin(editor);
     }
 }
+
